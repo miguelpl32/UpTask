4277e5083bfef256b8cc599988865a09c0514b9e
Miguel-Pariente <miguelpl32@hotmail.com>
2021-05-30 11:57:01 +0200

    Creacion Master page

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const path = require('path');
 //crear una app de express
 const app = express();
 
+// Carga de archivos estaticos
+app.use(express.static('public'));
+
 // Habilitar Pug 
 app.set('view engine', 'pug');
 
