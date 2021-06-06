6b74434ea590c1cb219426791cce89d8d9028281
Miguel-Pariente <miguelpl32@hotmail.com>
2021-06-06 12:13:53 +0200

    Creacion formulacion registrar usuarios

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const routes = require('./routes');
 const path = require('path');
-const bodyParser = require('body-parser');
+// const expressValidator = require('express-validator');
+const flash = require('connect-flash');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+
  
 // helpers con algunas funciones
 const helpers = require('./helpers');
@@ -9,9 +13,10 @@ const helpers = require('./helpers');
 // Crear la conexion a la BD
 const db = require('./config/db');
 
-// Importamos el model 
+// Importamos los modelos
 require('./models/Proyectos');
 require('./models/Tareas');
+require('./models/Usuarios');
 
 db.sync()
     .then(() => console.log('Conectado al Servidor'))
@@ -21,25 +26,42 @@ db.sync()
 //crear una app de express
 const app = express();
 
-
-
 // Carga de archivos estaticos
 app.use(express.static('public'));
 
 // Habilitar Pug 
 app.set('view engine', 'pug');
 
+// habilitar para leer datos del formulario
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+
+// app.use(expressValidator());
+
+
 // Añadir carpeta de las vistas
 app.set('views', path.join(__dirname, './views'));
+
+// agregar Flash messages
+app.use(flash());
+
+app.use(cookieParser());
+
+// Sesiones nos permite navegar entre distintas paginas sin volvernos a autenticar
+app.use(session({
+    secret: 'elsecreto',
+    resave: false,
+    saveUninitialized: false
+}));
 
 // Pasar var dump a la aplicacion
 app.use((req, res, next) => {
     res.locals.vardump = helpers.vardump;
+    res.locals.mensajes = req.flash();
     next();
 });
 
-// habilitar bodyParser para leer datos del formulario
-app.use(bodyParser.urlencoded({ extended: true }));
+
 
 app.use('/', routes());
 
--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,7 @@ const { body } = require('express-validator/check');
 //importar  controladores
 const proyectosController = require('../controllers/proyectosController');
 const tareasController = require('../controllers/tareasController');
+const usuariosController = require('../controllers/usuariosController');
 
 module.exports = function() {
     //ruta para el home 
@@ -37,6 +38,10 @@ module.exports = function() {
     
     // Eliminar Tarea 
     router.delete('/tareas/:id', tareasController.eliminarTarea);
+
+    // Crear nueva cuenta 
+    router.get('/crear-cuenta', usuariosController.formCrearCuenta);
+    router.post('/crear-cuenta', usuariosController.crearCuenta);
 
     return router;
 }
