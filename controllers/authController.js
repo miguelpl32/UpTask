6822aeef2df40a9755113f5f07c4e08a3c54ef2a
Miguel-Pariente <miguelpl32@hotmail.com>
2021-06-06 20:17:08 +0200

    Cerrar sesion y asegurar end-point

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,3 +6,22 @@ exports.autenticarUsuario = passport.authenticate('local', {
     failureFlash: true,
     badRequestMessage: 'Ambos Campos son Obligatorios'
 }); 
+
+// Funcion revisr si el usuario esta logueado o no 
+exports.usuarioAutenticado = (req, res, next) => {
+
+    // si el usuario esta usuarioAutenticado
+    if(req.isAuthenticated()) {
+        return next();
+    }
+    // sino esta autenticado, redirigir al formulario 
+    return res.redirect('/iniciar-sesion');
+
+}
+
+// Funcion para cerrar sesion 
+exports.cerrarSesion = (req, res) => {
+    req.session.destroy(() => {
+        res.redirect('/iniciar-sesion'); // al cerrar sesión nos lleva al login
+    })
+}
